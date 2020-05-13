f8ad50134a57c259205edcd507c0e726c5c6d3f4
Horki <ivanzvonimirhorvat@gmail.com>
2020-05-13 10:37:00 +0200

    test: lru; added leet test sample

--- test/test_caches.cc
+++ test/test_caches.cc
@@ -16,11 +16,11 @@ TEST(CACHE, test_get_by_same_key) {
 
 TEST(CACHE, test_no_exception) {
   LRUCache<int, int> lruCache(2);
-  lruCache.set(1, 2);
-  lruCache.set(2, 3);
-  lruCache.set(3, 4);
+  lruCache.set(1, 1);
+  lruCache.set(2, 2);
+  lruCache.set(3, 3);
   ASSERT_EQ(lruCache.get_opt(1), std::nullopt);
-  ASSERT_EQ(lruCache.get_opt(3), std::optional<int>(4));
+  ASSERT_EQ(lruCache.get_opt(3), std::optional<int>(3));
 }
 
 TEST(CACHE, test_out_of_range_error) {
@@ -40,4 +40,17 @@ TEST(CACHE, test_out_of_range_error) {
         }
       },
       std::range_error);
+}
+
+TEST(CACHE, test_leetcode) {
+  LRUCache<int, int> lruCache(2);
+  lruCache.set(1, 1);
+  lruCache.set(2, 2);
+  ASSERT_EQ(lruCache.get(1), 1);
+  lruCache.set(3, 3); // evicts key 2
+  ASSERT_EQ(lruCache.get_opt(2), std::nullopt);
+  lruCache.set(4, 4); // evicts key 1
+  ASSERT_EQ(lruCache.get_opt(1), std::nullopt);
+  ASSERT_EQ(lruCache.get(3), 3);
+  ASSERT_EQ(lruCache.get(4), 4);
 }
